608f821337915e537abfd3d10b5ae3bade374949
19029489 <19029489@rp.edu.sg>
2021-04-29 13:53:43 +0800

    Grade Class added and activity_main.xml edited

---
+++ app/src/main/java/com/myapplicationdev/android/twproblemstatementp02/Grade.java
@@ -0,0 +1,13 @@
+package com.myapplicationdev.android.twproblemstatementp02;
+
+public class Grade {
+    private String grade;
+
+    public Grade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+}
--- app/src/main/java/com/myapplicationdev/android/twproblemstatementp02/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/twproblemstatementp02/MainActivity.java
@@ -3,12 +3,36 @@ package com.myapplicationdev.android.twproblemstatementp02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvC347;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvC347 = (TextView)findViewById(R.id.textViewC347);
+
+        tvC347.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create Hero object of strength 100 & technical 60
+                Hero superman = new Hero("Superman", 100, 60);
+                Intent i = new Intent(MainActivity.this, HeroStatsActivity.class);
+
+                //put hero object in intent
+                i.putExtra("hero", superman);
+
+//                startActivity(i);
+                // Start activity with requestCodeForSupermanStats to
+                // identify it was started by clicking on Superman
+                startActivityForResult(i,requestCodeForSupermanStats);
+
+            }
+        });
     }
 }
