489eda23e4c78681583b36e32b830f0a5ab625ce
19022187 <19022187@myrp.edu.sg>
2021-04-29 15:52:04 +0800

    updated codes

--- app/src/main/java/com/myapplicationdev/android/twproblemstatementp02/ThirdActivity.java
+++ app/src/main/java/com/myapplicationdev/android/twproblemstatementp02/ThirdActivity.java
@@ -20,9 +20,6 @@ public class ThirdActivity extends AppCompatActivity {
 
     Button btnSubmit;
     RadioGroup rgGrade;
-    ListView lv;
-    ArrayAdapter aa;
-    ArrayList<Grade> alGrade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +28,10 @@ public class ThirdActivity extends AppCompatActivity {
 
         btnSubmit = (Button) findViewById(R.id.buttonSubmit);
 
-        lv = (ListView)findViewById(R.id.listView);
-
-        alGrade = new ArrayList<Grade>();
-        alGrade.add(new Grade("B"));
-        alGrade.add(new Grade("C"));
-        alGrade.add(new Grade("A"));
-
-        aa = new GradeAdapter(this, R.layout.row, alGrade);
-        lv.setAdapter(aa);
-
-
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent();
-                i.putExtra("Grade", rgGrade.isSelected());
-                setResult(RESULT_OK, i);
-                finish();
+
             }
         });
     }
