d4d8ae78ed85913034d3ea254e81726c869f0fc0
li tong <lililililitongg@gmail.com>
2021-04-29 14:30:20 +0800

    commit

--- app/src/main/java/com/myapplicationdev/android/twproblemstatementp02/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/twproblemstatementp02/SecondActivity.java
@@ -2,13 +2,27 @@ package com.myapplicationdev.android.twproblemstatementp02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-
+    Button btnEmail, btnRP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btnRP = (Button) findViewById(R.id.buttonInfo);
+        btnRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
     }
 }
